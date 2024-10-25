99d81234685833299adc26c0156ab85217d7ee90
sinian-liu <86273555+sinian-liu@users.noreply.github.com>
2024-10-25 17:38:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3355,7 +3355,7 @@ updateV2RayAgent() {
 	version=$(grep '当前版本:v' "/etc/v2ray-agent/install.sh" | awk -F "[v]" '{print $2}' | tail -n +2 | head -n 1 | awk -F "[\"]" '{print $1}')
 
 	echoContent green "\n ---> 更新完毕"
-	echoContent yellow " ---> 请手动执行[vasma]打开脚本"
+	echoContent yellow " ---> 请手动执行[sinian]打开脚本"
 	echoContent green " ---> 当前版本:${version}\n"
 	echoContent yellow "如更新不成功，请手动执行下面命令\n"
 	echoContent skyBlue "wget -P /root -N --no-check-certificate https://raw.githubusercontent.com/mack-a/v2ray-agent/master/install.sh && chmod 700 /root/install.sh && /root/install.sh"
